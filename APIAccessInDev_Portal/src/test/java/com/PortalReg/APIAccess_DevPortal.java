699db547a91c52271443421e01faaeb57993e8d0
Bhaskar.GVE <Bhaskar.GVE@DFALPT1087.DFA.local>
2020-05-05 14:45:40 +0200

    APIAccessInDev_Portal

---
+++ APIAccessInDev_Portal/src/test/java/com/PortalReg/APIAccess_DevPortal.java
@@ -0,0 +1,13 @@
+package com.PortalReg;
+
+import org.testng.annotations.Test;
+
+public class APIAccess_DevPortal {
+	@Test
+	public void login() throws InterruptedException
+	{
+		String url ="https://apidplab.dfafrica.co.za/admin/app/home";
+		WebDriver driver = null;
+		
+	}
+}
